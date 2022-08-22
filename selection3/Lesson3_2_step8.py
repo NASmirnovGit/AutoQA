17f4e34ab2b8edeee3cacacfabc7590ca9a90ea8
NASmirnovGit <nikus1727@gmail.com>
2022-08-22 11:23:16 +0700

    first commit

--- selection3/Lesson3_2_step8.py
+++ selection3/Lesson3_2_step8.py
@@ -2,4 +2,4 @@ def test_input_text(expected_result, actual_result):
     # ваша реализация, напишите assert и сообщение об ошибке
    assert expected_result in actual_result, \
         f"expected {expected_result}, got {actual_result}"
-# test_input_text( '123', '123')
+test_input_text( '123', '123')
