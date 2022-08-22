a76dbef6c386cf1547b16807c23c96dd7a0f9fca
NASmirnovGit <nikus1727@gmail.com>
2022-08-22 11:01:00 +0700

    first commit

--- selection3/Lesson3.2_step8.py
+++ selection3/Lesson3.2_step8.py
@@ -1,4 +1,5 @@
 def test_input_text(expected_result, actual_result):
     # ваша реализация, напишите assert и сообщение об ошибке
-   assert substring in full_string, \
+   assert expected_result in actual_result, \
         f"expected {expected_result}, got {actual_result}"
+# test_input_text( '123', '123')
